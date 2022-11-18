3209b0971ad252c1b587c4f4fec1f4015892353c
Chuan <cli@lambda-loginnode02.cm.cluster>
2022-11-18 13:05:09 -0600

    initial test with 64m, par and seq

---
+++ fio.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#TEST_SIZE=${1:-"64m"}
+#TEST_DIR=${2:-"/weka/cli/fio-test/fio_data/$HOSTNAME"}
+
+TEST_SIZE=64m
+RUNTIME=10s
+TEST_DIR="/weka/cli/fio-test/fio_data/$HOSTNAME"
+
+echo $TEST_SIZE
+echo $TEST_DIR
+
+mkdir -p $TEST_DIR
+
+date
+fio --name=write_throughput --directory=$TEST_DIR --numjobs=8  --size=$TEST_SIZE --time_based --runtime=$RUNTIME --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1
+date
+fio --name=write_iops --directory=$TEST_DIR --size=$TEST_SIZE --time_based --runtime=$RUNTIME --ramp_time=2s --ioengine=libaio --direct=1  --verify=0 --bs=4K --iodepth=64 --rw=randwrite --group_reporting=1
+date
+fio --name=read_throughput --directory=$TEST_DIR --numjobs=8  --size=$TEST_SIZE --time_based --runtime=$RUNTIME --ramp_time=2s --ioengine=libaio  --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=read --group_reporting=1
+date
+fio --name=read_iops --directory=$TEST_DIR --size=$TEST_SIZE --time_based --runtime=$RUNTIME --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=4K --iodepth=64 --rw=randread --group_reporting=1
+date
---
+++ run_par.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+for i in {001..025}
+do
+	ssh cli@lambda-hyperplane"$i" "/weka/cli/fio-test/fio.sh 2>&1 | tee /weka/cli/fio-test/fio_result/lambda-hyperplane"$i".txt" &
+done
